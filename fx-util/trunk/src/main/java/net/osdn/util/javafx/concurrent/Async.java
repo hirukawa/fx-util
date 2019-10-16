464e27f4eec1d4ae5a55823fe57fa678068f83ed
hirukawa_ryo <hirukawa_ryo@72ec87c8-6a28-4960-867d-f068561b4b27>
2019-10-16 01:05:41 +0000

    * fx-util 0.2.0 全面的に作り直しました。fx-util 0.1.1 との互換性はありません。

---
+++ fx-util/trunk/src/main/java/net/osdn/util/javafx/concurrent/Async.java
@@ -0,0 +1,268 @@
+package net.osdn.util.javafx.concurrent;
+
+import javafx.concurrent.Task;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class Async<V> implements AsyncRunnable, AsyncCallable<V> {
+
+	/**
+	 * タスク完了時の状態
+	 *
+	 */
+	public enum State {
+		/**
+		 * タスクがキャンセルされたことを示します。
+		 */
+		CANCELLED,
+		/**
+		 * タスクが正常に終了したことを示します。
+		 */
+		SUCCEEDED,
+		/**
+		 * 通常は予期しない条件が発生したことによって、タスクが失敗したことを示します。
+		 */
+		FAILED
+	}
+
+	private static ThreadFactory defaultThreadFactory;
+
+	/**
+	 * 新規スレッドの作成に使用するデフォルトのスレッド・ファクトリーを返します。
+	 *
+	 * @return スレッド・ファクトリー
+	 */
+	public static ThreadFactory getDefaultThreadFactory() {
+		return defaultThreadFactory;
+	}
+
+	/**
+	 * 新規スレッドの作成に使用するデフォルトのスレッド・ファクトリーを設定します。
+	 * スレッド・ファクトリーを設定するとエグゼキューターは再初期化されます。
+	 *
+	 * @param factory スレッド・ファクトリー
+	 */
+	public static void setDefaultThreadFactory(ThreadFactory factory) {
+		defaultThreadFactory = factory;
+		executor = null;
+	}
+
+	private static Executor executor;
+
+	/**
+	 * タスクを実行するエグゼキューターを返します。
+	 * エグゼキューターが明示的に設定されていない場合、
+	 * 既定のスレッド・ファクトリーを使ってスレッドをプールして再利用するエグゼキューターが新たに作成されます。
+	 * 既定のスレッド・ファクトリーが設定されていない場合、デーモン・スレッドを作成するファクトリーが使われます。
+	 *
+	 * @return エグゼキューター
+	 */
+	public static Executor getExecutor() {
+		if(executor == null) {
+			ThreadFactory factory = getDefaultThreadFactory();
+			if(factory == null) {
+				factory = new ThreadFactory() {
+					@Override
+					public Thread newThread(Runnable r) {
+						Thread thread = new Thread(r);
+						thread.setDaemon(true);
+						return thread;
+					}
+				};
+			}
+			executor = Executors.newCachedThreadPool(factory);
+		}
+		return executor;
+	}
+
+	/**
+	 * タスクを実行するエグゼキューターを設定します。
+	 * エグゼキューターが明示的に設定された場合、既定のスレッド・ファクトリーは使われません。
+	 *
+	 * @param executor エグゼキューター
+	 */
+	public static void setExecutor(Executor executor) {
+		Async.executor = executor;
+	}
+
+	/**
+	 * 指定したRunnableを非同期で実行します。
+	 *
+	 * @param runnable 非同期で実行する処理
+	 * @return メソッドチェーンでハンドラーを設定できます。
+	 */
+	public static AsyncRunnable execute(Runnable runnable) {
+		Async<Void> async = new Async<Void>(() -> {
+			runnable.run();
+			return null;
+		});
+		getExecutor().execute(async.task);
+		return async;
+	}
+
+	/**
+	 * 指定したCallableを非同期で実行します。
+	 * 処理は値を返すことができます。
+	 *
+	 * @param callable 非同期で実行する処理
+	 * @param <V> 処理の戻り値の型
+	 * @return メソッドチェーンでハンドラーを設定できます。
+	 */
+	public static <V> AsyncCallable<V> execute(Callable<V> callable) {
+		Async<V> async = new Async<V>(callable);
+		getExecutor().execute(async.task);
+		return async;
+	}
+
+	private Task<V> task;
+	private Cancel cancel;
+	private AsyncRunnable.Success runnableSuccess;
+	private AsyncCallable.Success<V> callableSuccess;
+	private Failure failure;
+	private Complete complete;
+
+	private Async(Callable<V> callable) {
+		task = new Task<V>() {
+			@Override
+			protected V call() throws Exception {
+				return callable.call();
+			}
+
+			@Override
+			protected void cancelled() {
+				Async.this.cancelled();
+			}
+
+			@Override
+			protected void succeeded() {
+				Async.this.succeeded();
+			}
+
+			@Override
+			protected void failed() {
+				Async.this.failed();
+			}
+		};
+	}
+
+	protected void cancelled() {
+		if(cancel != null) {
+			cancel.onCancelled();
+		}
+		if(complete != null) {
+			complete.onCompleted(Async.State.CANCELLED);
+		}
+	}
+
+	protected void succeeded() {
+		if(runnableSuccess != null) {
+			runnableSuccess.onSucceeded();
+		} else if(callableSuccess != null) {
+			callableSuccess.onSucceeded(task.getValue());
+		}
+		if(complete != null) {
+			complete.onCompleted(Async.State.SUCCEEDED);
+		}
+	}
+
+	protected void failed() {
+		if(failure != null) {
+			failure.onFailed(task.getException());
+		}
+		if(complete != null) {
+			complete.onCompleted(Async.State.FAILED);
+		}
+	}
+
+	/**
+	 * タスクを返します。
+	 *
+	 * @return タスク
+	 */
+	public Task<V> getTask() {
+		return this.task;
+	}
+
+	/**
+	 * タスクがキャンセルされたときに実行されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクがキャンセルされたときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	public Async<V> onCancelled(Cancel callback) {
+		this.cancel = callback;
+		return this;
+	}
+
+	/**
+	 * タスクが正常に終了したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクが正常に終了したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	public Async<V> onSucceeded(AsyncRunnable.Success callback) {
+		this.callableSuccess = null;
+		this.runnableSuccess = callback;
+		return this;
+	}
+
+	/**
+	 * タスクが正常に終了したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクが正常に終了したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	public Async<V> onSucceeded(AsyncCallable.Success<V> callback) {
+		this.runnableSuccess = null;
+		this.callableSuccess = callback;
+		return this;
+	}
+
+	/**
+	 * 例外がスローされてタスクが失敗したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクが失敗したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	public Async<V> onFailed(Failure callback) {
+		this.failure = callback;
+		return this;
+	}
+
+	/**
+	 * タスクが完了したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * タスクがキャンセルされた場合、タスクが正常に終了した場合、タスクが失敗した場合、
+	 * いずれの場合でも、最期にこのハンドラーが呼び出されます。
+	 *
+	 * @param callback タスクが完了したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	public Async<V> onCompleted(Complete callback) {
+		this.complete = callback;
+		return this;
+	}
+
+	@FunctionalInterface
+	public interface Cancel {
+		void onCancelled();
+	}
+
+	@FunctionalInterface
+	public interface Failure {
+		void onFailed(Throwable exception);
+	}
+
+	@FunctionalInterface
+	public interface Complete {
+		void onCompleted(State state);
+	}
+}
---
+++ fx-util/trunk/src/main/java/net/osdn/util/javafx/concurrent/AsyncCallable.java
@@ -0,0 +1,47 @@
+package net.osdn.util.javafx.concurrent;
+
+public interface AsyncCallable<V> {
+	/**
+	 * タスクがキャンセルされたときに実行されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクがキャンセルされたときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	AsyncCallable<V> onCancelled(Async.Cancel callback);
+
+	/**
+	 * タスクが正常に終了したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクが正常に終了したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	AsyncCallable<V> onSucceeded(Success<V> callback);
+
+	/**
+	 * 例外がスローされてタスクが失敗したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * @param callback タスクが失敗したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	AsyncCallable<V> onFailed(Async.Failure callback);
+
+	/**
+	 * タスクが完了したときに呼び出されるハンドラーを設定します。
+	 * ハンドラーはJavaFXアプリケーションスレッドで呼び出されます。
+	 *
+	 * タスクがキャンセルされた場合、タスクが正常に終了した場合、タスクが失敗した場合、
+	 * いずれの場合でも、最期にこのハンドラーが呼び出されます。
+	 *
+	 * @param callback タスクが完了したときに呼び出されるハンドラー
+	 * @return メソッドチェーンで他のハンドラーの設定を続けられます。
+	 */
+	AsyncCallable<V> onCompleted(Async.Complete callback);
+
+	@FunctionalInterface
+	public interface Success<T> {
+		void onSucceeded(T result);
+	}
+}
