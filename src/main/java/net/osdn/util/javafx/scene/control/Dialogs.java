850577d46cd54cd0dc14cdf8356167a6dbfba602
hirukawa_ryo <hirukawa_ryo@72ec87c8-6a28-4960-867d-f068561b4b27>
2019-12-05 04:51:32 +0000

    * fx-util 0.3.0 ステージの位置とサイズを復元可能にするStageUtil.setRestorableメソッドで、最大化時に誤った位置が保存されるバグを修正しました。 Dialogs.showConfirmationで表示されるダイアログのボタンを OK/CANCEL から YES/NO に変更しました。 ダイアログの既定のCANCELボタンのキャプションを「取消」から「キャンセル」になるようにようにしました。

--- src/main/java/net/osdn/util/javafx/scene/control/Dialogs.java
+++ src/main/java/net/osdn/util/javafx/scene/control/Dialogs.java
@@ -1,6 +1,11 @@
 package net.osdn.util.javafx.scene.control;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.MissingResourceException;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -9,12 +14,39 @@ import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class Dialogs {
+
+	protected static final Map<ButtonType, String> buttonTexts = new HashMap<ButtonType, String>();
+
+	static {
+		String BASE_NAME = "com/sun/javafx/scene/control/skin/resources/controls";
+		Map<ButtonType, String> keys = new LinkedHashMap<ButtonType, String>();
+		keys.put(ButtonType.APPLY, "Dialog.apply.button");
+		keys.put(ButtonType.OK, "Dialog.ok.button");
+		keys.put(ButtonType.CANCEL, "Dialog.cancel.button");
+		keys.put(ButtonType.CLOSE, "Dialog.close.button");
+		keys.put(ButtonType.YES, "Dialog.yes.button");
+		keys.put(ButtonType.NO, "Dialog.no.button");
+		keys.put(ButtonType.FINISH, "Dialog.finish.button");
+		keys.put(ButtonType.NEXT, "Dialog.next.button");
+		keys.put(ButtonType.PREVIOUS, "Dialog.previous.button");
+
+		ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME);
+		if(bundle != null) {
+			for(Map.Entry<ButtonType, String> entry : keys.entrySet()) {
+				try {
+					buttonTexts.put(entry.getKey(), bundle.getString(entry.getValue()));
+				} catch(MissingResourceException ignore) {}
+			}
+		}
+	}
 
 	public static ButtonType showInformation(String message) {
 		return show(AlertType.INFORMATION, null, null, null, message, new ButtonType[] { ButtonType.OK });
@@ -33,19 +65,19 @@ public class Dialogs {
 	}
 
 	public static ButtonType showConfirmation(String message) {
-		return show(AlertType.CONFIRMATION, null, null, null, message, new ButtonType[] { ButtonType.OK, ButtonType.CANCEL });
+		return show(AlertType.CONFIRMATION, null, null, null, message, new ButtonType[] { ButtonType.YES, ButtonType.NO });
 	}
 
 	public static ButtonType showConfirmation(String title, String message) {
-		return show(AlertType.CONFIRMATION, null, null, title, message, new ButtonType[] { ButtonType.OK, ButtonType.CANCEL });
+		return show(AlertType.CONFIRMATION, null, null, title, message, new ButtonType[] { ButtonType.YES, ButtonType.NO });
 	}
 
 	public static ButtonType showConfirmation(Window owner, String message) {
-		return show(AlertType.CONFIRMATION, owner, null, null, message, new ButtonType[] { ButtonType.OK, ButtonType.CANCEL });
+		return show(AlertType.CONFIRMATION, owner, null, null, message, new ButtonType[] { ButtonType.YES, ButtonType.NO });
 	}
 
 	public static ButtonType showConfirmation(Window owner, String title, String message) {
-		return show(AlertType.CONFIRMATION, owner, null, title, message, new ButtonType[] { ButtonType.OK, ButtonType.CANCEL });
+		return show(AlertType.CONFIRMATION, owner, null, title, message, new ButtonType[] { ButtonType.YES, ButtonType.NO });
 	}
 
 	public static ButtonType showWarning(String message) {
@@ -118,6 +150,23 @@ public class Dialogs {
 				}
 			});
 		}
+
+		dialog.showingProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if(newValue) {
+					DialogPane dialogPane = dialog.getDialogPane();
+					for(ButtonType buttonType : dialogPane.getButtonTypes()) {
+						String text = buttonTexts.get(buttonType);
+						if(text != null) {
+							Button button = (Button)dialogPane.lookupButton(buttonType);
+							button.setText(text);
+						}
+					}
+					dialog.showingProperty().removeListener(this);
+				}
+			}
+		});
 
 		Optional<ButtonType> result = dialog.showAndWait();
 		return result.isPresent() ? result.get() : null;
