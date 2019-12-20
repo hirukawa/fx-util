44a6f57e8ccef7b134945383eebdb57f007ba657
hirukawa_ryo <hirukawa_ryo@72ec87c8-6a28-4960-867d-f068561b4b27>
2019-12-20 01:58:27 +0000

    * fx-util 0.3.3 コンテキストメニューからマウスカーソルが外れたときの振る舞い修正のバグを修正しました。（メニューアイテムを動的に追加した場合にリスナーが登録されず正しく動作しませんでした。）

--- fx-util/trunk/src/main/java/net/osdn/util/javafx/scene/control/ContextMenuUtil.java
+++ fx-util/trunk/src/main/java/net/osdn/util/javafx/scene/control/ContextMenuUtil.java
@@ -1,8 +1,12 @@
 package net.osdn.util.javafx.scene.control;
 
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -15,7 +19,12 @@ import java.util.WeakHashMap;
 
 public class ContextMenuUtil {
 
-	private static Map<ContextMenu, EventHandler<WindowEvent>> fixEventHandlers = new WeakHashMap<ContextMenu, EventHandler<WindowEvent>>();
+	private static Map<ObservableList<Menu>, ListChangeListener<Menu>> menuListChangeListeners = new WeakHashMap<>();
+	private static Map<ObservableList<MenuItem>, ListChangeListener<MenuItem>> menuItemListChangeListeners = new WeakHashMap<>();
+	private static Map<ReadOnlyObjectProperty<ContextMenu>, ChangeListener<ContextMenu>> parentPopupChangeListeners = new WeakHashMap<>();
+	private static Map<ContextMenu, EventHandler<WindowEvent>> contextMenuWindowShowingEventHandlers = new WeakHashMap<>();
+	private static Map<Node, EventHandler<MouseEvent>> menuItemNodeMouseExitedEventHandlers = new WeakHashMap<>();
+	private static Map<Node, EventHandler<MouseEvent>> menuItemNodeMouseReleasedEventFilters = new WeakHashMap<>();
 
 	/**
 	 * 指定したメニューバーに関連するコンテキスト・メニューの振る舞いを修正します。
@@ -27,7 +36,28 @@ public class ContextMenuUtil {
 		if(menuBar == null) {
 			return null;
 		}
-		for(Menu menu : menuBar.getMenus()) {
+		ObservableList<Menu> menus = menuBar.getMenus();
+		ListChangeListener<Menu> oldListChangeListener = menuListChangeListeners.get(menus);
+		if(oldListChangeListener != null) {
+			menus.removeListener(oldListChangeListener);
+			menuListChangeListeners.remove(menus);
+		}
+		ListChangeListener<Menu> newListChangeListener = new ListChangeListener<Menu>() {
+			@Override
+			public void onChanged(Change<? extends Menu> c) {
+				while(c.next()) {
+					if(c.wasAdded()) {
+						for(Menu menu : c.getAddedSubList()) {
+							fix(menu);
+						}
+					}
+				}
+			}
+		};
+		menus.addListener(newListChangeListener);
+		menuListChangeListeners.put(menus, newListChangeListener);
+
+		for(Menu menu : menus) {
 			fix(menu);
 		}
 		return menuBar;
@@ -43,7 +73,28 @@ public class ContextMenuUtil {
 		if(menu == null) {
 			return null;
 		}
-		for(MenuItem menuItem : menu.getItems()) {
+		ObservableList<MenuItem> menuItems = menu.getItems();
+		ListChangeListener<MenuItem> oldListChangeListener = menuItemListChangeListeners.get(menuItems);
+		if(oldListChangeListener != null) {
+			menuItems.removeListener(oldListChangeListener);
+			menuItemListChangeListeners.remove(menuItems);
+		}
+		ListChangeListener<MenuItem> newListChangeListener = new ListChangeListener<MenuItem>() {
+			@Override
+			public void onChanged(Change<? extends MenuItem> c) {
+				while(c.next()) {
+					if(c.wasAdded()) {
+						for(MenuItem menuItem : c.getAddedSubList()) {
+							fix(menuItem);
+						}
+					}
+				}
+			}
+		};
+		menuItems.addListener(newListChangeListener);
+		menuItemListChangeListeners.put(menuItems, newListChangeListener);
+
+		for(MenuItem menuItem : menuItems) {
 			fix(menuItem);
 		}
 		return menu;
@@ -59,17 +110,26 @@ public class ContextMenuUtil {
 		if(menuItem == null) {
 			return null;
 		}
-		menuItem.parentPopupProperty().addListener((observable, oldValue, newValue) -> {
-			if(oldValue != null) {
-				EventHandler<WindowEvent> oldEventHandler = fixEventHandlers.get(oldValue);
-				if(oldEventHandler != null) {
-					oldValue.removeEventHandler(WindowEvent.WINDOW_SHOWING, oldEventHandler);
+		ReadOnlyObjectProperty<ContextMenu> parentPopup = menuItem.parentPopupProperty();
+		ChangeListener<ContextMenu> oldChangeListener = parentPopupChangeListeners.get(parentPopup);
+		if(oldChangeListener != null) {
+			parentPopup.removeListener(oldChangeListener);
+			parentPopupChangeListeners.remove(parentPopup);
+		}
+		ChangeListener<ContextMenu> newChangeListener = new ChangeListener<ContextMenu>() {
+			@Override
+			public void changed(ObservableValue<? extends ContextMenu> observable, ContextMenu oldValue, ContextMenu newValue) {
+				if(oldValue != null) {
+					removeListeners(oldValue);
+				}
+				if(newValue != null) {
+					fix(newValue, false);
 				}
 			}
-			if(newValue != null) {
-				fix(newValue, false);
-			}
-		});
+		};
+		parentPopup.addListener(newChangeListener);
+		parentPopupChangeListeners.put(parentPopup, newChangeListener);
+
 		return menuItem;
 	}
 
@@ -108,9 +168,10 @@ public class ContextMenuUtil {
 		if(contextMenu == null) {
 			return null;
 		}
-		EventHandler<WindowEvent> oldEventHandler = fixEventHandlers.get(contextMenu);
+		EventHandler<WindowEvent> oldEventHandler = contextMenuWindowShowingEventHandlers.get(contextMenu);
 		if(oldEventHandler != null) {
 			contextMenu.removeEventHandler(WindowEvent.WINDOW_SHOWING, oldEventHandler);
+			contextMenuWindowShowingEventHandlers.remove(contextMenu);
 		}
 		EventHandler<WindowEvent> newEventHandler = new EventHandler<WindowEvent>() {
 			@Override
@@ -118,29 +179,88 @@ public class ContextMenuUtil {
 				Node contextMenuNode = contextMenu.getStyleableNode();
 				for (MenuItem menuItem : contextMenu.getItems()) {
 					Node menuItemNode = menuItem.getStyleableNode();
-					if(menuItemNode == null) {
-						continue;
-					}
-					menuItemNode.setOnMouseExited(me -> {
-						contextMenuNode.requestFocus();
-					});
-					menuItemNode.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
-						if(!menuItemNode.isFocused()) {
-							e.consume();
-							if(hideOnMouseReleased) {
-								contextMenu.hide();
-							}
-						}
-					});
+					fixImpl(contextMenu, contextMenuNode, menuItem, menuItemNode, hideOnMouseReleased);
 				}
 				// コンテキスト・メニュー表示時になぜか先頭のアイテムが選択状態になることがあるようなので、
 				// コンテキスト・メニュー自体にフォーカス要求を出して、先頭アイテムが初期選択状態にならないようにします。
 				contextMenuNode.requestFocus();
 				contextMenu.removeEventHandler(WindowEvent.WINDOW_SHOWING, this);
+				contextMenuWindowShowingEventHandlers.remove(contextMenu);
 			}
 		};
 		contextMenu.addEventHandler(WindowEvent.WINDOW_SHOWING, newEventHandler);
-		fixEventHandlers.put(contextMenu, newEventHandler);
+		contextMenuWindowShowingEventHandlers.put(contextMenu, newEventHandler);
 		return contextMenu;
+	}
+
+	private static void fixImpl(ContextMenu contextMenu, Node contextMenuNode, MenuItem menuItem, Node menuItemNode, boolean hideOnMouseReleased) {
+		if(contextMenu == null) {
+			return;
+		}
+		if(contextMenuNode == null) {
+			return;
+		}
+		if(menuItem == null) {
+			return;
+		}
+		if(menuItemNode == null) {
+			return;
+		}
+
+		removeListeners(menuItemNode);
+
+		EventHandler<MouseEvent> newEventHandler = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				contextMenuNode.requestFocus();
+			}
+		};
+		menuItemNode.addEventHandler(MouseEvent.MOUSE_EXITED, newEventHandler);
+		menuItemNodeMouseExitedEventHandlers.put(menuItemNode, newEventHandler);
+
+		EventHandler<MouseEvent> newEventFilter = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(!menuItemNode.isFocused()) {
+					event.consume();
+					if(hideOnMouseReleased) {
+						contextMenu.hide();
+					}
+				}
+			}
+		};
+		menuItemNode.addEventFilter(MouseEvent.MOUSE_RELEASED, newEventFilter);
+		menuItemNodeMouseReleasedEventFilters.put(menuItemNode, newEventFilter);
+	}
+
+	private static void removeListeners(ContextMenu contextMenu) {
+		if(contextMenu == null) {
+			return;
+		}
+		for(MenuItem menuItem : contextMenu.getItems()) {
+			Node menuItemNode = menuItem.getStyleableNode();
+			removeListeners(menuItemNode);
+		}
+		EventHandler<WindowEvent> oldEventHandler = contextMenuWindowShowingEventHandlers.get(contextMenu);
+		if(oldEventHandler != null) {
+			contextMenu.removeEventHandler(WindowEvent.WINDOW_SHOWING, oldEventHandler);
+			contextMenuWindowShowingEventHandlers.remove(contextMenu);
+		}
+	}
+
+	private static void removeListeners(Node menuItemNode) {
+		if(menuItemNode == null) {
+			return;
+		}
+		EventHandler<MouseEvent> oldEventHandler = menuItemNodeMouseExitedEventHandlers.get(menuItemNode);
+		if(oldEventHandler != null) {
+			menuItemNode.removeEventHandler(MouseEvent.MOUSE_EXITED, oldEventHandler);
+			menuItemNodeMouseExitedEventHandlers.remove(menuItemNode);
+		}
+		EventHandler<MouseEvent> oldEventFilter = menuItemNodeMouseReleasedEventFilters.get(menuItemNode);
+		if(oldEventFilter != null) {
+			menuItemNode.removeEventFilter(MouseEvent.MOUSE_RELEASED, oldEventFilter);
+			menuItemNodeMouseReleasedEventFilters.remove(menuItemNode);
+		}
 	}
 }
