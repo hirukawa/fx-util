711171024894d19e2fae4331279289919afa9c12
hirukawa_ryo <hirukawa_ryo@72ec87c8-6a28-4960-867d-f068561b4b27>
2021-02-10 06:36:04 +0000

    * fx-util 0.4.1 Stageの位置等を記憶する方法としてPreferencesではなくPropertiesも指定できるようにしました。 WindowsのPreferences実装はレジストリをストアとして使用します。レジストリではなくファイルに状態を保存したい場合はPropertiesを指定してください。 ただし、StageUtilはPropertiesのファイル保存はおこないません。別途、Propertiesをファイルに保存する必要があります。

--- src/main/java/net/osdn/util/javafx/stage/StageUtil.java
+++ src/main/java/net/osdn/util/javafx/stage/StageUtil.java
@@ -9,9 +9,109 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Properties;
 import java.util.prefs.Preferences;
 
 public class StageUtil {
+
+	private static boolean getBoolean(Properties properties, String key, boolean defaultValue) {
+		String value = properties.getProperty(key);
+		try {
+			if(value != null) {
+				return Boolean.parseBoolean(value);
+			}
+		} catch(Exception ignore) {}
+		return defaultValue;
+	}
+
+	private static void putBoolean(Properties properties, String key, boolean value) {
+		properties.put(key, Boolean.toString(value));
+	}
+
+	private static double getDouble(Properties properties, String key, double defaultValue) {
+		String value = properties.getProperty(key);
+		try {
+			if(value != null) {
+				return Double.parseDouble(value);
+			}
+		} catch(Exception ignore) {}
+		return defaultValue;
+	}
+
+	private static void putDouble(Properties properties, String key, double value) {
+		properties.put(key, Double.toString(value));
+	}
+
+	/**
+	 * ステージの位置とサイズを復元可能にします。
+	 *
+	 * @param stage ステージ
+	 * @param properties プロパティ―
+	 */
+	public static void setRestorable(Stage stage, Properties properties) {
+		double x = getDouble(properties, "stageX", stage.getX());
+		double y = getDouble(properties, "stageY", stage.getY());
+		double w = getDouble(properties,"stageWidth", stage.getWidth());
+		double h = getDouble(properties,"stageHeight", stage.getHeight());
+		boolean maximized = getBoolean(properties, "stageMaximized", false);
+		Rectangle2D workarea = Screen.getPrimary().getVisualBounds();
+		if(x + w > workarea.getMaxX()) {
+			x = workarea.getMaxX() - w;
+		}
+		if(x < workarea.getMinX()) {
+			x = workarea.getMinX();
+		}
+		if(x + w > workarea.getMaxX()) {
+			w = workarea.getMaxX() - x;
+		}
+		if(y + h > workarea.getMaxY()) {
+			y = workarea.getMaxY() - h;
+		}
+		if(y < workarea.getMinY()) {
+			y = workarea.getMinY();
+		}
+		if(y + h > workarea.getMaxY()) {
+			h = workarea.getMaxY() - y;
+		}
+		stage.setX(x);
+		stage.setY(y);
+		stage.setWidth(w);
+		stage.setHeight(h);
+		stage.setMaximized(maximized);
+
+		stage.xProperty().addListener((observable, oldValue, newValue) -> {
+			// 最大化操作をしたときに、stage.isMaximized が true になる前に x が左上座標に変更されてしまいます。
+			// 最大化操作時に x 座標が保存されないように Platform.runLater で判定を遅延させます。
+			// Platform.runLater で指定したタスクは最大化が完了した後に呼び出されるため、stage.isMaximized が有意な値になっています。
+			// これは x, y に必要な対処です。width, height の変更は isMaximized の変更より後でおこなわれるため、
+			// Platform.runLater で遅延させなくても正しく最大化状態を判定できます。
+			Platform.runLater(() -> {
+				if(!stage.isMaximized() && !stage.isFullScreen() && !stage.isIconified()) {
+					putDouble(properties,"stageX", newValue.doubleValue());
+				}
+			});
+		});
+		stage.yProperty().addListener((observable, oldValue, newValue) -> {
+			Platform.runLater(() -> {
+				if(!stage.isMaximized() && !stage.isFullScreen() && !stage.isIconified()) {
+					putDouble(properties, "stageY", newValue.doubleValue());
+				}
+			});
+		});
+		stage.widthProperty().addListener((observable, oldValue, newValue) -> {
+			if(!stage.isMaximized() && !stage.isFullScreen() && !stage.isIconified()) {
+				putDouble(properties, "stageWidth", newValue.doubleValue());
+			}
+		});
+		stage.heightProperty().addListener((observable, oldValue, newValue) -> {
+			if(!stage.isMaximized() && !stage.isFullScreen() && !stage.isIconified()) {
+				putDouble(properties, "stageHeight", newValue.doubleValue());
+			}
+		});
+		stage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
+			putBoolean(properties,"stageMaximized", newValue.booleanValue());
+		});
+	}
 
 	/**
 	 * ステージの位置とサイズを復元可能にします。
