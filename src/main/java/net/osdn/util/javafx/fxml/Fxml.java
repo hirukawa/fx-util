1289b19031da65043fff86833c015b2452190fb5
hirukawa_ryo <hirukawa_ryo@72ec87c8-6a28-4960-867d-f068561b4b27>
2021-05-25 14:50:27 +0000

    * fx-util 0.4.3 macOS で FXMLLoader.getDefaultClassLoader() が null になってしまう問題への回避策を追加しました。

--- src/main/java/net/osdn/util/javafx/fxml/Fxml.java
+++ src/main/java/net/osdn/util/javafx/fxml/Fxml.java
@@ -13,40 +13,70 @@ import java.io.UncheckedIOException;
 public class Fxml {
 
     public static <T> T load(Class<?> cls) {
-        String fxmlFilename = cls.getSimpleName() + ".fxml";
-        FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            String fxmlFilename = cls.getSimpleName() + ".fxml";
+            FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
     }
 
     public static <T> T load(Object root, Class<?> cls) {
-        String fxmlFilename = cls.getSimpleName() + ".fxml";
-        FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
-        loader.setRoot(root);
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            String fxmlFilename = cls.getSimpleName() + ".fxml";
+            FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
+            loader.setRoot(root);
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
     }
 
     public static <T> T load(Object controller) {
-        String fxmlFilename = controller.getClass().getSimpleName() + ".fxml";
-        FXMLLoader loader = new FXMLLoader(controller.getClass().getResource(fxmlFilename));
-        loader.setController(controller);
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            String fxmlFilename = controller.getClass().getSimpleName() + ".fxml";
+            FXMLLoader loader = new FXMLLoader(controller.getClass().getResource(fxmlFilename));
+            loader.setController(controller);
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
     }
 
@@ -60,32 +90,52 @@ public class Fxml {
             throw new NullPointerException();
         }
 
-        String fxmlFilename = cls.getSimpleName() + ".fxml";
-        FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
-        if(root != null) {
-            loader.setRoot(root);
-        }
-        if(controller != null) {
-            loader.setController(controller);
-        }
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            String fxmlFilename = cls.getSimpleName() + ".fxml";
+            FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
+            if(root != null) {
+                loader.setRoot(root);
+            }
+            if(controller != null) {
+                loader.setController(controller);
+            }
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
     }
 
     public static <T> T load(Object controller, String fxmlFilename) {
-        FXMLLoader loader = new FXMLLoader(controller.getClass().getResource(fxmlFilename));
-        loader.setController(controller);
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            FXMLLoader loader = new FXMLLoader(controller.getClass().getResource(fxmlFilename));
+            loader.setController(controller);
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
     }
 
@@ -99,20 +149,57 @@ public class Fxml {
             throw new NullPointerException();
         }
 
-        FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
-        if(root != null) {
-            loader.setRoot(root);
-        }
-        if(controller != null) {
-            loader.setController(controller);
-        }
+        boolean isContextClassLoaderReplaced = false;
         try {
-            T obj = loader.load();
-            fix(obj);
-            return obj;
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            isContextClassLoaderReplaced = setContextClassLoaderIfNeeded();
+
+            FXMLLoader loader = new FXMLLoader(cls.getResource(fxmlFilename));
+            if(root != null) {
+                loader.setRoot(root);
+            }
+            if(controller != null) {
+                loader.setController(controller);
+            }
+            try {
+                T obj = loader.load();
+                fix(obj);
+                return obj;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
+        } finally {
+            if(isContextClassLoaderReplaced) {
+                Thread.currentThread().setContextClassLoader(null);
+            }
         }
+    }
+
+
+    private static boolean setContextClassLoaderIfNeeded() {
+        //
+        // macOS で FXML のロードに失敗するバグ対策です。
+        //
+        // Windows でもユーザーから報告されたスタックトレースで同様の事象が発生することがあるようです。
+        // Caused by: java.lang.NullPointerException
+        //     at javafx.fxml/javafx.fxml.JavaFXBuilderFactory.<init>(Unknown Source)
+        //     at javafx.fxml/javafx.fxml.JavaFXBuilderFactory.<init>(Unknown Source)
+        //     at javafx.fxml/javafx.fxml.FXMLLoader.<clinit>(Unknown Source)
+        //
+        // JavaFXBuilderFactoryのコンストラクタで NullPointerException が発生するのは FXMLLoader.getDefaultClassLoader() が null を返している可能性が考えられます。
+        // FXMLLoader.getDefaultClassLoader() は FXMLLoader.setDefaultClassLoader( ) で明示的にクラスローダーが設定されていない場合、
+        // Thread.currentThread().getContextClassLoader() を返します。これが null になっている可能性が考えられます。
+        // その対策として Thread.currentThread().getContextClassLoader() が null の場合、システムクラスローダーを設定します。
+        // ※ これが原因であるとの確証、および改善の保証はまだありません。
+        //
+        if(Thread.currentThread().getContextClassLoader() == null) {
+            ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+            if(systemClassLoader != null) {
+                Thread.currentThread().setContextClassLoader(systemClassLoader);
+                return true;
+            }
+        }
+        return false;
     }
 
     private static void fix(Object obj) {
