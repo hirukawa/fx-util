2f041e23aaf0c24f9668ec464db42ad1b1e80ea8
hirukawa <g1t217-hu8503>
2024-04-18 01:00:42 +0900

    バージョン 0.6.2 SceneUtil.invokeAfterAnimationImpl isNeedsLayout が false になるまで PostLayoutPulseListener の発生を待ち、その後、PreLayoutPulseListener の発生を待つようにしました。これでノードの画面表示（のほぼ直前）まで待機できるようです。 SceneUtil.invokeAfterAnimationImpl 万が一、PostLayoutPulseListener, PreLayoutPulseListener が呼び出されなかった場合に備えてタイムアウトでも runnable を呼び出すようにしました。

--- src/main/java/onl/oss/javafx/scene/SceneUtil.java
+++ src/main/java/onl/oss/javafx/scene/SceneUtil.java
@@ -1,31 +1,46 @@
 package onl.oss.javafx.scene;
 
 import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.util.Duration;
 
 public class SceneUtil {
 
 	/** 指定したノードのレイアウト完了後に runnable を実行します。
+	 * 既定の時間（500ミリ秒）が経過してもレイアウトが完了していない場合も runnable を実行します。
 	 *
 	 * @param parent ノード
 	 * @param runnable レイアウト完了後に実行する処理
 	 */
 	public static void invokeAfterLayout(Parent parent, Runnable runnable) {
+		invokeAfterLayout(parent, runnable, null);
+	}
+
+	/** 指定したノードのレイアウト完了後に runnable を実行します。
+	 * 指定した時間が経過してもレイアウトが完了していない場合も runnable を実行します。
+	 *
+	 * @param parent ノード
+	 * @param runnable レイアウト完了後に実行する処理
+	 * @param timeout タイムアウト
+	 */
+	public static void invokeAfterLayout(Parent parent, Runnable runnable, Duration timeout) {
 		if(Platform.isFxApplicationThread()) {
-			invokeAfterLayoutImpl(parent, runnable);
+			invokeAfterLayoutImpl(parent, runnable, timeout);
 		} else {
 			Platform.runLater(() -> {
-				invokeAfterLayoutImpl(parent, runnable);
+				invokeAfterLayoutImpl(parent, runnable, timeout);
 			});
 		}
 	}
 
-	private static void invokeAfterLayoutImpl(Parent parent, Runnable runnable) {
+	private static void invokeAfterLayoutImpl(Parent parent, Runnable runnable, Duration timeout) {
 		if(parent == null) {
 			throw new NullPointerException();
 		}
@@ -39,15 +54,46 @@ public class SceneUtil {
 			throw new NullPointerException();
 		}
 
-		scene.addPostLayoutPulseListener(new Runnable() {
+		final boolean[] isComplated = new boolean[1];
+
+		final Runnable preLayoutPulseListener = new Runnable() {
+			@Override
+			public void run() {
+				scene.removePreLayoutPulseListener(this);
+
+				if (!isComplated[0]) {
+					isComplated[0] = true;
+					runnable.run();
+				}
+			}
+		};
+
+		final Runnable postLayoutPulseListener = new Runnable() {
 			@Override
 			public void run() {
 				if(!parent.isNeedsLayout()) {
 					scene.removePostLayoutPulseListener(this);
-					runnable.run();
+					scene.addPreLayoutPulseListener(preLayoutPulseListener);
+					Platform.requestNextPulse();
 				}
 			}
-		});
+		};
+
+		if (timeout == null) {
+			timeout = Duration.millis(500);
+		}
+
+		new Timeline(new KeyFrame(timeout, event -> {
+			if (!isComplated[0]) {
+				System.out.println("SceneUtil.invokeAfterLayout timeout: node=" + parent);
+				isComplated[0] = true;
+				scene.removePostLayoutPulseListener(postLayoutPulseListener);
+				scene.removePreLayoutPulseListener(preLayoutPulseListener);
+				runnable.run();
+			}
+		})).play();
+
+		scene.addPostLayoutPulseListener(postLayoutPulseListener);
 		Platform.requestNextPulse();
 	}
 
